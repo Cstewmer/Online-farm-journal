3409e24caa621190222bba65739280fbe5217c2e
Charles Stewart Merriam <159678108+Cstewmer@users.noreply.github.com>
2025-07-13 05:19:55 +0000

    Rhetorical Analysis and visual fixes

--- src/App.tsx
+++ src/App.tsx
@@ -26,14 +26,13 @@ function App() {
           <h2>Introduction to EAP (Equine Assisted Psychotherapy)</h2>
           <div className="introduction-content">
             <div className="introduction-text">
-              <p>Equine-Assisted Psychotherapy (referred to hereafter as EAP) is an emerging yet increasingly popular form of therapy used as a tool either independently or in conjunction with other methods "that helps clients of all ages address behavioral, emotional, spiritual, and relational issues using horses as an interventional tool." (Mandrell, 2014) While it's difficult to say exactly when working with horses was first used as a mental health tool (as humans have been working with horses for decades), the idea was widely popularized by a book called "The Horse Boy", published in 2009 by Rupert Isaacson. Since then, numerous studies on the effectiveness of EAP have been and continue to be performed, with many showing positive results. (Burgon, 2015)</p>
-            </div>
-            <div className="introduction-image">
-              <img 
-                src="./introduction-image.jfif" 
-                alt="Equine Therapy Introduction" 
-                className="intro-image"
-              />
+              <p>
+                <img 
+                  src="./introduction-image.jfif" 
+                  alt="Equine Therapy Introduction" 
+                  className="intro-image-float"
+                />Equine-Assisted Psychotherapy (referred to hereafter as EAP) is an emerging yet increasingly popular form of therapy used as a tool either independently or in conjunction with other methods "that helps clients of all ages address behavioral, emotional, spiritual, and relational issues using horses as an interventional tool." (Mandrell, 2014) While it's difficult to say exactly when working with horses was first used as a mental health tool (as humans have been working with horses for decades), the idea was widely popularized by a book called "The Horse Boy", published in 2009 by Rupert Isaacson. Since then, numerous studies on the effectiveness of EAP have been and continue to be performed, with many showing positive results. (Burgon, 2015)
+              </p>
             </div>
           </div>
           <p>While recently EAP has become recognizable to a wider audience, there are still many misconceptions surrounding the practice, and as such I'd like to begin my background by clarifying what EAP isn't. It isn't therapy for the horse that happens to benefit the human working with it, it isn't physical therapy or hippotherapy (which is a method of therapy aiming to treat neurological/muscular disorders using the movements involved in riding a horse), and it isn't instilling character in an adolescent by tasking them with the responsibilities of raising a horse. (Mandrell, 2014)</p>
@@ -45,10 +44,31 @@ function App() {
           <p>EAP is young and emerging, but as more and more research is conducted, the benefits of using this tool in psychotherapy become increasingly clear. </p>
         </div>
       </div>
+      <div className="rhetorical-analysis-container">
+        <div className="rhetorical-analysis">
+          <h2>Rhetorical Analysis</h2>
+          <img 
+            src="./Good Meme.jpg" 
+            alt="Rhetorical Analysis" 
+            className="rhetorical-analysis-image rhetorical-analysis-image-small"
+          />
+          <p>The image I have chose to analyze is the cartoon found above. While probably not intended as a malicious attack against EAP (in fact, the author probably thinks they're advocating for it), it is a great example of how misconeptions around Equine-Assisted pyscotherapy contribute to a legitimate method not being taken seriously among professionals and not considered by clients looking for a solution.</p>
+          <br />
+          <p>The first misconception this image is propegating has to do with ethos and the sign on the top, reading "The Doctor is in". This implies that the horse itself is responsible for the positive outcomes associated with psychotherapy. But, as previously stated, the horse is merely one tool that can be used by the real professional (a trained and licensed psycotherapist) in order to treat a patient. Simply spending time alone with a horse will not provide lasting improvements in ones emotional habits/coping strategies. The author IS trying to establish ethos, but is trying to frame the horse as the repuatable expert. This, along with the cartoonish depiction of the horse, does less to establish EAP as a credible and science-backed method and instead gives more ammunition to those who would write EAP off as an excuse for horse-lovers to waste more time with animals isntead of getting professional help.</p>
+          <br /> 
+          <p>The second way this image is unintentionally influencing would-be clients is the two lines that read "Therapeutic Riding". Though the intention of the author is most likely to provide some expectations of what EAP looks like and provide some logical backing to their argument (who wouldn't find a relaxing horse ride therapeutic?), they're once again undermining the legitimacy of the practice. Logically speaking, it makes no sense for a truly troubled person to magically become better after a soothing horse-ride through the mountains. A truly effective element of logos would be something more along the lines of "EAP Session with licensed psycotherapist" - this would have influenced logical thinkers more positively towards EAP instead of away from it.</p>
+          <br />
+          <p>Finally, the line at the bottom - "Cuddles - Unlimited". This is obviously an attempt to influence the audience using pathos, but once again it misses the mark. This implies EAP uses the horse as some kind of emotional support animal - a concept that has already become controversial in the media for being questionably effective and frequently misused (Herzog, 2023) Portraying EAP as some kind of feel-good cuddle session with a horse might work with an extremely narrow audience, but it will likely turn away anyone who is looking for a legitimate therapy method that isn't already obsessed with cuddling horses and greatly disappoint those who are - in EAP sessions there is very little of "cuddling" with the horse.</p>
+          <br />
+          <p>In conclusion, there IS material advocating for EAP and it IS effectively influencing audiences, but not in the direction they want. By portraying EAP as silly, cuddly, and playing around with a horse all by yourself, it's scaring off people who could legitimately benefit from the method.</p>
+        </div>
+      </div>
       <div className="sources-container">
         <div className="sources">
           <h2>Sources Cited</h2>
           <p>Burgon, H. L. (2015). Equine-assisted therapy and learning with at-risk young people Hannah Burgon. Palgrave Macmillan.</p>
+          <br />
+          <p>Herzog, H. (2023, February 13). The use and misuse of emotional support animals. Psychology Today. https://www.psychologytoday.com/us/blog/animals-and-us/202302/the-use-and-misuse-of-emotional-support-animals</p>
           <br />
           <p>Mandrell, P. J. (2014). Introduction to equine-assisted psychotherapy: A comprehensive overview. The Author.</p>
           <br />
