90260ec6ad142e9f86bedd895fcd336d22b6d22b
Charles Stewart Merriam <159678108+Cstewmer@users.noreply.github.com>
2025-07-20 04:31:11 +0000

    Add Labor section with certification requirements and update sources

--- src/App.tsx
+++ src/App.tsx
@@ -52,15 +52,45 @@ function App() {
             alt="Rhetorical Analysis" 
             className="rhetorical-analysis-image rhetorical-analysis-image-small"
           />
+          <p className="image-citation">Image found on Pinterest. Candace Kimbrough: https://www.pinterest.com/pin/35817759508797248/</p>
+          <br />
           <p>The image I have chose to analyze is the cartoon found above. While probably not intended as a malicious attack against EAP (in fact, the author probably thinks they're advocating for it), it is a great example of how misconeptions around Equine-Assisted pyscotherapy contribute to a legitimate method not being taken seriously among professionals and not considered by clients looking for a solution.</p>
           <br />
-          <p>The first misconception this image is propegating has to do with ethos and the sign on the top, reading "The Doctor is in". This implies that the horse itself is responsible for the positive outcomes associated with psychotherapy. But, as previously stated, the horse is merely one tool that can be used by the real professional (a trained and licensed psycotherapist) in order to treat a patient. Simply spending time alone with a horse will not provide lasting improvements in ones emotional habits/coping strategies. The author IS trying to establish ethos, but is trying to frame the horse as the repuatable expert. This, along with the cartoonish depiction of the horse, does less to establish EAP as a credible and science-backed method and instead gives more ammunition to those who would write EAP off as an excuse for horse-lovers to waste more time with animals isntead of getting professional help.</p>
+          <p>The first misconception this image is propegating has to do with ethos and the sign on the top, reading "The Doctor is in". This implies that the horse itself is responsible for the positive outcomes associated with psychotherapy. But, as previously stated, the horse is merely one tool that can be used by the real professional (a trained and licensed psycotherapist) in order to treat a patient (Mandrell, 2014). Simply spending time alone with a horse will not provide lasting improvements in ones emotional habits/coping strategies. The author IS trying to establish ethos, but is trying to frame the horse as the repuatable expert. This, along with the cartoonish depiction of the horse, does less to establish EAP as a credible and science-backed method and instead gives more ammunition to those who would write EAP off as an excuse for horse-lovers to waste more time with animals isntead of getting professional help.</p>
           <br /> 
           <p>The second way this image is unintentionally influencing would-be clients is the two lines that read "Therapeutic Riding". Though the intention of the author is most likely to provide some expectations of what EAP looks like and provide some logical backing to their argument (who wouldn't find a relaxing horse ride therapeutic?), they're once again undermining the legitimacy of the practice. Logically speaking, it makes no sense for a truly troubled person to magically become better after a soothing horse-ride through the mountains. A truly effective element of logos would be something more along the lines of "EAP Session with licensed psycotherapist" - this would have influenced logical thinkers more positively towards EAP instead of away from it.</p>
           <br />
           <p>Finally, the line at the bottom - "Cuddles - Unlimited". This is obviously an attempt to influence the audience using pathos, but once again it misses the mark. This implies EAP uses the horse as some kind of emotional support animal - a concept that has already become controversial in the media for being questionably effective and frequently misused (Herzog, 2023) Portraying EAP as some kind of feel-good cuddle session with a horse might work with an extremely narrow audience, but it will likely turn away anyone who is looking for a legitimate therapy method that isn't already obsessed with cuddling horses and greatly disappoint those who are - in EAP sessions there is very little of "cuddling" with the horse.</p>
           <br />
           <p>In conclusion, there IS material advocating for EAP and it IS effectively influencing audiences, but not in the direction they want. By portraying EAP as silly, cuddly, and playing around with a horse all by yourself, it's scaring off people who could legitimately benefit from the method.</p>
+        </div>
+      </div>
+      <div className="labor-container">
+        <div className="labor">
+          <h2>Labor</h2>
+          <p>There are three professionals involved in the EAP process - a licensed pyscotherapist that has completed a certification from an accreddited EAP institution, an equine specialist that has done the same, and a carefully selected horse.</p>
+          <br />
+          <p>First, the Mental Health Professional must meet the following criteria –</p>
+          <ul>
+            <li>A graduate degree in a mental health field</li>
+            <li>Be practicing under a governing body.</li>
+          </ul>
+          <p>As they practice EAP, they must continue to stay within the scope of their practice and education. Once they've obtained the proper background education, they can enroll in a certification program from a reputable institution (such as EGALA) in order to be certified as a specialized Equine Assisted Mental Health Professional. These certifications must be renewed every two years. They then can find an equally qualified Equine Specialist partner and begin practicing Equine Assisted Therapy. (EAGALA, Certifications page)</p>
+          <br />
+          <p>The Equine Specialist must be at least 21 years of age and obtain a similar certification through a reputable program. The process of becoming certified as an Equine Specialist that's qualified to work with a Mental Health Professional includes becoming CPR and first aid certified, completing both online and on-site trainings, and at least 60 hours of paid or volunteer experience with a current Equine Specialist. (PATH, Certifications Page)</p>
+          <br />
+          <p>It's also possible to be certified as either a MHP or ES while obtaining your formal education. Utah State offers an Equine-Assisted Services Institutional Certificate of Proficiency that can be obtained while pursuing another degree. (Equine: Advs. USU.)</p>
+          <br />
+          <p>While in theory any horse can be used in EAP, Equine Specialists are taught during their certification that careful selection of the horse is important to ensure the safety of the client and the best results are realized. It's important that the horse meets the following criteria:</p>
+          <ul>
+            <li>Be sound at the walk, trot, and canter</li>
+            <li>Be well trained both under saddle and in hand</li>
+            <li>Be comfortable with sudden noises, unbalanced riders, and positional changes</li>
+            <li>Be able to live outside with other horses</li>
+            <li>Be free of vices such as biting, kicking, bucking, or rearing</li>
+            <li>Be up to date on vaccinations, dentals, and farrier work</li>
+          </ul>
+          <p>(Healing Reins Equine-Assisted Services)</p>
         </div>
       </div>
       <div className="sources-container">
@@ -73,6 +103,14 @@ function App() {
           <p>Mandrell, P. J. (2014). Introduction to equine-assisted psychotherapy: A comprehensive overview. The Author.</p>
           <br />
           <p>Whitborne, S. K. (2022, May 25). Equine-assisted therapy. Psychology Today. https://www.psychologytoday.com/us/therapy-types/equine-assisted-therapy</p>
+          <br />
+          <p>Certification information | PATH INTL. Path International Certifications Page. (n.d.). https://pathintl.org/certification/</p>
+          <br />
+          <p>Therapy horse criteria. Healing Reins Equine Assisted Services. (2022, August 4). https://healingreins.org/therapy-horse-criteria/#:~:text=Be%20sound%20at%20the%20walk,vaccinations%2C%20dentals%2C%20and%20farrier%20work</p>
+          <br />
+          <p>How to get certified in the EAGALA model of equine therapy. Eagala. (2025, June 24). https://www.eagala.org/blog/how-to-become-a-certified-equine-assisted-psychotherapist/#:~:text=Pursue%20relevant%20education%20in%20fields,qualify%20to%20certify%20with%20Eagala.</p>
+          <br />
+          <p>University, U. S. (n.d.). EAS: Equine: Advs. USU. https://agnr.usu.edu/advs/equine/equine-assisted-services/certificate</p>
         </div>
       </div>
     </div>
